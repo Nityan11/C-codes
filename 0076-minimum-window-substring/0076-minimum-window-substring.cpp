7e94ccd0daa460c90594a7dc8f0f5eddc087edeb
pygmy_puff25 <55041196+Nityan11@users.noreply.github.com>
2022-10-23 01:02:14 +0530

    Time: 48 ms (37.70%), Space: 18.8 MB (5.70%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,52 @@
+#include<climits>
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.size() == 0 || t.size() == 0 || t.size() > s.size()) {
+            return "";
+        }
+        int count = 0;
+        unordered_map<char,int> tmap;
+        for(int i = 0; i<t.size(); i++) {
+            tmap[t[i]]++;
+        }
+        //filtered s
+        vector<pair<char,int>> filter;
+        for(int i = 0; i<s.size(); i++){
+            if(tmap.count(s[i])){
+                pair<char,int> p(s[i],i);
+                filter.push_back(p);
+            }
+            
+        }
+        int minlength = INT_MAX;
+        string ans;
+        int i = 0; int j = 0;
+        
+        while(j<filter.size()) {
+            
+            tmap[filter[j].first]--;
+            if(tmap[filter[j].first]>=0) {
+                count++;
+            }
+            
+            while(count == t.size()) {
+                if(filter[j].second-filter[i].second+1<minlength) {
+                    minlength = filter[j].second-filter[i].second+1;
+                    ans = s.substr(filter[i].second,minlength);
+                }
+                //if(tmap.count(filter[i].first)) {
+                    tmap[filter[i].first]++;
+                    if(tmap[filter[i].first]>0){
+                        count--;
+                    }
+                //}
+                i++;
+            }
+        
+            j++;
+        }
+        
+        return ans;
+    }
+};
